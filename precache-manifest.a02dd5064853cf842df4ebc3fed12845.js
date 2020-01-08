82f63093d080640ad4535e0367209f92e824c193
vishalkrchopra <vishal.mca.mnnit@gmail.com>
2020-01-08 14:54:06 +0530

    Updates

---
+++ precache-manifest.a02dd5064853cf842df4ebc3fed12845.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "106566047f5415fa8180",
+    "url": "/Typing-Test/static/css/main.832d2379.chunk.css"
+  },
+  {
+    "revision": "106566047f5415fa8180",
+    "url": "/Typing-Test/static/js/main.10656604.chunk.js"
+  },
+  {
+    "revision": "2a5faa87ac820130ab05",
+    "url": "/Typing-Test/static/js/runtime~main.2a5faa87.js"
+  },
+  {
+    "revision": "289d44ad562f45000135",
+    "url": "/Typing-Test/static/js/2.289d44ad.chunk.js"
+  },
+  {
+    "revision": "587478bfef2fcb44f4d00c72a86218a9",
+    "url": "/Typing-Test/static/media/logo.587478bf.png"
+  },
+  {
+    "revision": "e0b7c898511d6b89c91aed5ae6d9762e",
+    "url": "/Typing-Test/static/media/GoreSir.e0b7c898.jpeg"
+  },
+  {
+    "revision": "8a1bed959bed2605ea1b829a517e54a1",
+    "url": "/Typing-Test/static/media/vishal.8a1bed95.jpg"
+  },
+  {
+    "revision": "d5042e466309a999f40946479d96767e",
+    "url": "/Typing-Test/index.html"
+  }
+];
